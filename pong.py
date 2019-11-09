4b7eb8794cd48ae3f99a33ae1e4e8f1d1f574543
tpin1001 <t.pin1001@gmail.com>
2019-11-09 02:05:52 -0500

    Added the ping pong ball

--- pong.py
+++ pong.py
@@ -28,6 +28,14 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
 # Main game loop
 while True:
     wn.update()
