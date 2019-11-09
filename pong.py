30508e63dbba5d76ebcbd978d93e6dcf46bd5938
tpin1001 <t.pin1001@gmail.com>
2019-11-09 02:37:19 -0500

    Border checking

--- pong.py
+++ pong.py
@@ -36,7 +36,7 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = .15
-ball.dy = .15
+ball.dy = -.15
 
 # Function
 def paddle_a_up():
@@ -74,3 +74,23 @@ while True:
     # move the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    
+    # Paddle and ball collision
