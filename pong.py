7a2e55fb531ffddbf08929f6c78d7db3f34c0087
tpin1001 <t.pin1001@gmail.com>
2019-11-09 01:46:32 -0500

    Setup the display screen on game

---
+++ pong.py
@@ -0,0 +1,11 @@
+# Simple Pon in Python 3.7 for beginners
+# By @t_pin1001
+# Part 1: Getting Started
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @t_pin1001")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(n=0)
