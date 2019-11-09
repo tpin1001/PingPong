4c5af6f380c55fad9b0edd3eee168dece31b3a75
tpin1001 <t.pin1001@gmail.com>
2019-11-09 02:18:57 -0500

    Added ball movement

--- pong.py
+++ pong.py
@@ -35,6 +35,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
 
 # Function
 def paddle_a_up():
@@ -42,8 +44,33 @@ def paddle_a_up():
     y += 20
     paddle_a.sety(y)
 
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 
 # Main game loop
 while True:
     wn.update()
+
+    # move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor + ball.dy)
