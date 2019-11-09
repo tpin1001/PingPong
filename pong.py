9703e50c40cb1385bdfa5e8714a354b7dee90f1e
tpin1001 <t.pin1001@gmail.com>
2019-11-09 02:52:06 -0500

    Added ball movement and collusion

--- pong.py
+++ pong.py
@@ -94,3 +94,10 @@ while True:
 
     
     # Paddle and ball collision
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+    
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
